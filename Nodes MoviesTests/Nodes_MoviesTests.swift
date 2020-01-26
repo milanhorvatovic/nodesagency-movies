eb3bd50bec79f50cb97b7a8b6fad89174292a73a
Milan Horvatovic <imsoft.labs@gmail.com>
2020-01-26 18:45:13 +0100

    [+] empty project

---
+++ Nodes MoviesTests/Nodes_MoviesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Nodes_MoviesTests.swift
+//  Nodes MoviesTests
+//
+//  Created by Milan Horvatovic on 26/01/2020.
+//  Copyright © 2020 Milan Horvatovic. All rights reserved.
+//
+
+import XCTest
+@testable import Nodes_Movies
+
+class Nodes_MoviesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
