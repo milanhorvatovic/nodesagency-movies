c652ecb25dd80758c3e7cc985cd55efdd4d43ee6
Milan Horvatovic <imsoft.labs@gmail.com>
2020-01-26 22:33:03 +0100

    [+] favorite data provider

---
+++ Nodes Movies/Application/Favorite/FavoriteDataProvider.swift
@@ -0,0 +1,134 @@
+//
+//  FavoriteDataProvider.swift
+//  Nodes Movies
+//
+//  Created by Milan Horvatovic on 26/01/2020.
+//  Copyright © 2020 Milan Horvatovic. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+import RxRelay
+import Action
+
+protocol FavoriteDataProvider {
+
+    var favoriteAction: Action<Model.Service.Movie.Detail, [Model.Service.Movie.Detail]> { get }
+    var unfavoriteAction: Action<Model.Service.Movie.Detail, [Model.Service.Movie.Detail]> { get }
+
+    func providerFavirites() -> Observable<[Model.Service.Movie.Detail]>
+
+}
+
+final class DefaultFavoriteDataProvider: FavoriteDataProvider {
+
+    private(set) lazy var favorited: BehaviorRelay<[Model.Service.Movie.Detail]> = self.createFavorited()
+
+    private(set) lazy var favoriteAction: Action<Model.Service.Movie.Detail, [Model.Service.Movie.Detail]> = {
+        return Action(workFactory: { [unowned self] (movie) -> Observable<[Model.Service.Movie.Detail]> in
+            return Observable.just(movie)
+                .withLatestFrom(self.favorited, resultSelector: { ($0, $1) })
+                .filter({ (movie, movies) -> Bool in
+                    return !movies.contains(movie)
+                })
+                .map({ (movie, movies) -> [Model.Service.Movie.Detail] in
+                    var movies = movies
+                    movies.append(movie)
+                    return movies
+                })
+        })
+    }()
+
+    var unfavoriteAction: Action<Model.Service.Movie.Detail, [Model.Service.Movie.Detail]> {
+        return Action(workFactory: { [unowned self] (movie) -> Observable<[Model.Service.Movie.Detail]> in
+            return Observable.just(movie)
+                .withLatestFrom(self.favorited, resultSelector: { ($0, $1) })
+                .filter({ (movie, movies) -> Bool in
+                    return movies.contains(movie)
+                })
+                .map({ (movie, movies) -> [Model.Service.Movie.Detail] in
+                    var movies = movies
+                    movies.removeAll(where: { (element) -> Bool in
+                        return element == movie
+                    })
+                    return movies
+                })
+        })
+    }
+
+    private let disposeBag: DisposeBag
+
+    init() {
+        self.disposeBag = DisposeBag()
+
+        self.favoriteAction.elements
+            .distinctUntilChanged()
+            .bind(to: self.favorited)
+            .disposed(by: self.disposeBag)
+        self.unfavoriteAction.elements
+            .distinctUntilChanged()
+            .bind(to: self.favorited)
+            .disposed(by: self.disposeBag)
+        self.favorited
+            .distinctUntilChanged()
+            .subscribe(onNext: { [unowned self] (favorited) in
+                do {
+                    try self.store(favorited)
+                }
+                catch {
+                    fatalError("Storing was unsuccessful")
+                }
+            })
+            .disposed(by: self.disposeBag)
+    }
+
+    func providerFavirites() -> Observable<[Model.Service.Movie.Detail]> {
+        return .just([])
+    }
+
+}
+
+extension DefaultFavoriteDataProvider {
+
+    private func createFavorited() -> BehaviorRelay<[Model.Service.Movie.Detail]> {
+        do {
+            return try BehaviorRelay(value: self.load())
+        }
+        catch {
+            return BehaviorRelay(value: [])
+        }
+    }
+
+}
+
+extension DefaultFavoriteDataProvider {
+
+    private static let key: String = "objects"
+
+    private func storage() -> UserDefaults {
+        guard let storage = UserDefaults(suiteName: "Favorited") else {
+            fatalError("Storage couldn't be initialized")
+        }
+        return storage
+    }
+
+    private func load() throws -> [Model.Service.Movie.Detail] {
+        let storage = self.storage()
+        guard let data = storage.data(forKey: type(of: self).key) else {
+            return []
+        }
+        let decoder = JSONDecoder()
+        return try decoder.decode([Model.Service.Movie.Detail].self,
+                                  from: data)
+    }
+
+    private func store(_ objects: [Model.Service.Movie.Detail]) throws {
+        let storage = self.storage()
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(objects)
+        storage.set(data,
+                    forKey: type(of: self).key)
+    }
+
+}
